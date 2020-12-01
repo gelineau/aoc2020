2f1c48a8e538c2cc9816b88979d4386ca49aab7e
Francois Gelineau <francois.gelineau@orange.com>
2020-12-01 10:59:34 +0100

    Add day 1

---
+++ d1-p1.py
@@ -0,0 +1,13 @@
+from typing import Set
+
+filename = "d1-p1.txt"
+
+def read_input()->Set[int]:
+    with open(filename, "r", encoding="utf-8") as file:
+        return set(int(value) for value in file)
+
+input = read_input()
+
+for value1 in input:
+    if (2020 - value1) in input:
+        print(f"value1: {value1}, value2: {2020 - value1}, product: {value1*(2020 - value1)}")
---
+++ d1-p2.py
@@ -0,0 +1,30 @@
+from typing import Set, Iterator, Tuple
+
+filename = "d1-p1.txt"
+
+
+def read_input() -> Set[int]:
+    with open(filename, "r", encoding="utf-8") as file:
+        return set(int(value) for value in file)
+
+
+def find_two_numbers(input: Set[int], sum: int) -> Iterator[int]:
+    for value1 in input:
+        if (sum - value1) in input:
+            yield value1
+
+
+input = read_input()
+
+
+def find_three_numbers(input: Set[int], sum: int) -> Iterator[
+    Tuple[int, int, int]]:
+    for value1 in input.copy():
+        input.remove(value1)
+        results = find_two_numbers(input, sum - value1)
+        for value2 in results:
+            yield value2, sum - value1 - value2, value1,
+
+
+results = find_three_numbers(input, 2020)
+print(list(a * b * c for (a, b, c) in results))
