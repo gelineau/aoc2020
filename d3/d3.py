bb2506fd35212476a132f47ce611da398edf0dcb
Francois Gelineau <francois.gelineau@orange.com>
2020-12-03 09:11:10 +0100

    Reformat code

--- d3/d3.py
+++ d3/d3.py
@@ -7,15 +7,19 @@ tree = "#"
 
 filename = "puzzle.txt"
 
-def read_input()->List[str]:
+
+def read_input() -> List[str]:
     with open(filename, "r", encoding="utf-8") as file:
         return file.readlines()
 
-def is_tree(maze: List[str], row: int, column: int)->bool:
+
+def is_tree(maze: List[str], row: int, column: int) -> bool:
     reduced_column = column % width
     return maze[row][reduced_column] == tree
 
-def tree_number(maze: List[str], height: int, down_slope: int, right_slope: int):
+
+def tree_number(maze: List[str], height: int, down_slope: int,
+                right_slope: int):
     tree_number = 0
     for step, row in enumerate(range(0, height, down_slope)):
         column = step * right_slope
@@ -24,6 +28,7 @@ def tree_number(maze: List[str], height: int, down_slope: int, right_slope: int)
 
     return tree_number
 
+
 maze = read_input()
 height = len(maze)
 
@@ -31,9 +36,9 @@ right_slope = 3
 down_slope = 1
 
 slopes = [(1, 1),
-          (3,1),
-          (5,1),
-          (7,1),
+          (3, 1),
+          (5, 1),
+          (7, 1),
           (1, 2)]
 
 result = 1
@@ -41,5 +46,3 @@ for right_slope, down_slope in slopes:
     result *= tree_number(maze, height, down_slope, right_slope)
 
 print(result)
-
-
