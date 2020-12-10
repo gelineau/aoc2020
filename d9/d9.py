b1aff25bc1870e2da5e7dfb8105c1272d3e0841c
Francois Gelineau <francois.gelineau@orange.com>
2020-12-10 17:32:58 +0100

    Add day 9  part 2

--- d9/d9.py
+++ d9/d9.py
@@ -20,4 +20,27 @@ for index, number in enumerate(input[25:], start=25):
     window = set(input[index-25:index])
     if all((number - candidate) not in window for candidate in window if candidate != number / 2):
         print(number, "is not a sum")
+        break
 
+invalid_number = number
+
+
+def find_sequence(invalid_number, input):
+    sums_so_far = {} # key = sum, value = corresponding sequence
+    for number in input:
+
+        new_sums_so_far = {}
+        for existing_sum, existing_numbers in list(sums_so_far.items()) + [
+            (0, [])]:
+            if number + existing_sum == invalid_number:
+                return(existing_numbers + [number])
+
+            if number + existing_sum < invalid_number:
+                new_sums_so_far[number + existing_sum] = existing_numbers + [
+                    number]
+
+        sums_so_far = new_sums_so_far
+
+
+sequence = find_sequence(invalid_number, input)
+print(min(sequence) + max(sequence))
